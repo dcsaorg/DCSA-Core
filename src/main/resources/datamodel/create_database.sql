e5ab956f01b1378c2edef4314191c87307ea4671
Asger Stage <as@asseco.dk>
2020-07-21 15:03:10 +0200

    asdf

--- src/main/resources/datamodel/create_database.sql
+++ src/main/resources/datamodel/create_database.sql
@@ -3,4 +3,10 @@
 
 DROP DATABASE IF EXISTS dcsa_openapi;
 CREATE DATABASE dcsa_openapi ENCODING = 'UTF8' ;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; -- Used to generate UUIDs
+CREATE SCHEMA dcsa_v1_1;
+CREATE SCHEMA dcsa_v2_0;
+GRANT ALL PRIVILEGES ON DATABASE dcsa_openapi TO dcsa_db_owner;
+GRANT ALL PRIVILEGES ON SCHEMA dcsa_v1_1 TO dcsa_db_owner;
+GRANT ALL PRIVILEGES ON SCHEMA dcsa_v2_0 TO dcsa_db_owner;
 
