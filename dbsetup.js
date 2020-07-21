a09a5f88e36d0b988ba440b32f58f182c6370ffc
Asger Stage <as@asseco.dk>
2020-07-21 11:16:40 +0200

    update dbscript

--- dbsetup.js
+++ dbsetup.js
@@ -3,17 +3,17 @@ const { Client } = require('pg');
 const pgclient = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
-    user: 'dcsa_',
+    user: 'dcsa_db_owner',
     password: 'postgres',
     database: 'dcsa_openapi'
 });
 
 pgclient.connect();
 
-const createdb = const dataSql = fs.readFileSync('.src/main/resources/datamodel/setup_databases.sql').toString();
-const dcsa_tnt_v1 = const dataSql = fs.readFileSync('.src/main/resources/datamodel/dcsa_tnt_v1.sql').toString();
-const dcsa_v2 = const dataSql = fs.readFileSync('.src/main/resources/datamodel/dcsa_v2.sql').toString();
-const data = const dataSql = fs.readFileSync('.src/main/resources/datamodel/test_data.sql').toString();
+const createdb =   fs.readFileSync('.src/main/resources/datamodel/setup_databases.sql').toString();
+const dcsa_tnt_v1 =  fs.readFileSync('.src/main/resources/datamodel/dcsa_tnt_v1.sql').toString();
+const dcsa_v2 =  fs.readFileSync('.src/main/resources/datamodel/dcsa_v2.sql').toString();
+const data =  fs.readFileSync('.src/main/resources/datamodel/test_data.sql').toString();
 
 pgclient.query(createdb, (err, res) => {
     if (err) throw err
