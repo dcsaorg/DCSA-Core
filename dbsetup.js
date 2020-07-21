57362bee82b482e9b77cfb3026237d4770404431
Asger Stage <as@asseco.dk>
2020-07-21 14:57:41 +0200

    asdf

--- dbsetup.js
+++ dbsetup.js
@@ -7,13 +7,6 @@ const dcsa_tnt_v1 =  fs.readFileSync('src/main/resources/datamodel/dcsa_tnt_v1.s
 const dcsa_v2 =  fs.readFileSync('src/main/resources/datamodel/dcsa_v2.sql').toString();
 const data =  fs.readFileSync('src/main/resources/datamodel/test_data.sql').toString();
 
-const dcsaclient = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'dcsa_openapi'
-});
 
 const pgclient = new Client({
     host: process.env.POSTGRES_HOST,
@@ -26,35 +19,23 @@ const pgclient = new Client({
 pgclient.connect();
 
 
-console.log('hej');
-
-pgclient.query("CREATE DATABASE dcsa_openapi ENCODING = 'UTF8' ;", (err, res) => {
-    if (err) throw err
-});
-console.log('hej');
-pgclient.end()
-console.log('hej');
-
-
-dcsaclient.connect();
-
-dcsaclient.query(setupdb, (err, res) => {
+pgclient.query(setupdb, (err, res) => {
     if (err) throw err
 });
 
-dcsaclient.query(dcsa_tnt_v1, (err, res) => {
+pgclient.query(dcsa_tnt_v1, (err, res) => {
     if (err) throw err
 });
 
-dcsaclient.query(dcsa_v2, (err, res) => {
+pgclient.query(dcsa_v2, (err, res) => {
     if (err) throw err
 });
-dcsaclient.query(data, (err, res) => {
+pgclient.query(data, (err, res) => {
     if (err) throw err
 });
 
 
-dcsaclient.query('SELECT * FROM dcsa_v1_1.events', (err, res) => {
+pgclient.query('SELECT * FROM dcsa_v1_1.events', (err, res) => {
     if (err) throw err
     console.log(err, res.rows) // Print data
     dcsaclient.end()
