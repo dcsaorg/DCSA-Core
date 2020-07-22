546d15b0d28517a41ca60dd27044f1add93cc8d8
Asger Stage <as@asseco.dk>
2020-07-22 11:26:29 +0200

    sdfa

--- src/main/resources/datamodel/create_database.sql
+++ src/main/resources/datamodel/create_database.sql
@@ -7,6 +7,7 @@ CREATE DATABASE dcsa_openapi ENCODING = 'UTF8' ;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; -- Used to generate UUIDs
 CREATE SCHEMA dcsa_v1_1;
 CREATE SCHEMA dcsa_v2_0;
+CREATE ROLE dcsa_db_owner;
 GRANT ALL PRIVILEGES ON DATABASE dcsa_openapi TO dcsa_db_owner;
 GRANT ALL PRIVILEGES ON SCHEMA dcsa_v1_1 TO dcsa_db_owner;
 GRANT ALL PRIVILEGES ON SCHEMA dcsa_v2_0 TO dcsa_db_owner;
