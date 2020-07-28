6ea675f6c2ec1cd7070fe5988de348391062e1bb
Asger Stage <as@asseco.dk>
2020-07-28 11:51:27 +0200

    remove comments

--- src/main/java/org/dcsa/model/EventSubscription.java
+++ src/main/java/org/dcsa/model/EventSubscription.java
@@ -4,18 +4,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.dcsa.model.enums.EventType;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import javax.persistence.Entity;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.sql.Date;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 @Table("event_subscription")
 @Data
@@ -38,34 +32,6 @@ public class EventSubscription extends AuditBase implements GetId<String> {
     @JsonProperty("eventType")
     private String eventType;
 
-//    public List<EventType> getEventType() {
-//        return Objects.requireNonNullElseGet(eventType, () -> convertCommaSeparatedStringToEnumList(eventTypes));
-//    }
-
-
-//    public String getEventTypes() {
-//        eventTypes= eventType.stream()
-//              .map(EventType::toString)
-//              .collect(Collectors.joining(","));
-//        return eventTypes;
-//    }
-//
-//    public void setEventTypes(String eventTypes) {
-//        this.eventTypes=eventTypes;
-//        this.eventType=convertCommaSeparatedStringToEnumList(eventTypes);
-//    }
-//
-//    public void setEventType(List<EventType> eventType) {
-//        this.eventType=eventType;
-//        this.eventTypes= eventType.stream()
-//              .map(EventType::toString)
-//              .collect(Collectors.joining(","));
-//    }
-
-
-//    @JsonProperty("eventType")
-//    private List<EventType> eventType;
-
     @JsonProperty("bookingReference")
     @Column("booking_reference")
     private String bookingReference;
@@ -78,13 +44,4 @@ public class EventSubscription extends AuditBase implements GetId<String> {
     @Column("equipment_reference")
     private String equipmentReference;
 
-    private List<EventType> convertCommaSeparatedStringToEnumList(String commaSeparatedList) {
-        List<EventType> eventTypesEnumList = new ArrayList<>();
-        if (commaSeparatedList != null && !commaSeparatedList.isEmpty()) {
-            for (String type : commaSeparatedList.split(",")) {
-                eventTypesEnumList.add(EventType.valueOf(type));
-            }
-        }
-        return eventTypesEnumList;
-    }
 }
