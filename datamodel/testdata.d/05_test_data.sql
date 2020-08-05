b99c234db63f32d8bc3e6bc3144ab083b994fd16
Asger Stage <as@asseco.dk>
2020-08-05 13:14:33 +0200

    test

--- datamodel/testdata.d/05_test_data.sql
+++ datamodel/testdata.d/05_test_data.sql
@@ -159,4 +159,4 @@ INSERT INTO dcsa_v1_1.event_subscription(
     bill_of_lading_number,
     equipment_reference
 )
-    VALUES ('172.17.0.1:4567/webhook/receive-transport-events', 'TRANSPORT', '', '', '');
+    VALUES ('0.0.0.0:4567/webhook/receive-transport-events', 'TRANSPORT', '', '', '');
