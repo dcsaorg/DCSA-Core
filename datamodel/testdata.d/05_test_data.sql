6be044d81326bff61bb9106dbae189c33d3fe11a
Asger Stage <as@asseco.dk>
2020-08-05 15:57:25 +0200

    add protocol to data

--- datamodel/testdata.d/05_test_data.sql
+++ datamodel/testdata.d/05_test_data.sql
@@ -140,7 +140,7 @@ INSERT INTO dcsa_v1_1.event_subscription(
     bill_of_lading_number,
     equipment_reference
 )
-    VALUES ('localhost:4567/webhook/receive', '', '', '', '');
+    VALUES ('http://localhost:4567/webhook/receive', '', '', '', '');
 
     INSERT INTO dcsa_v1_1.event_subscription(
     callback_url,
@@ -149,4 +149,4 @@ INSERT INTO dcsa_v1_1.event_subscription(
     bill_of_lading_number,
     equipment_reference
 )
-    VALUES ('localhost:4567/webhook/receive-transport-events', 'TRANSPORT', '', '', '');
+    VALUES ('http://localhost:4567/webhook/receive-transport-events', 'TRANSPORT', '', '', '');
