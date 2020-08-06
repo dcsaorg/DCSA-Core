2931635756fd62a1d477f527c139a2660bc9d88a
Nicolas Guilbert <nicolas@ange.dk>
2020-08-06 20:32:42 +0200

    Add shipment test data (TNT v2.0)

--- datamodel/initdb.d/04_dcsa_v2.sql
+++ datamodel/initdb.d/04_dcsa_v2.sql
@@ -65,12 +65,12 @@ CREATE TABLE dcsa_v2_0.schedule (
 DROP TABLE IF EXISTS dcsa_v2_0.shipment_event CASCADE;
 CREATE TABLE dcsa_v2_0.shipment_event (
     id uuid NOT NULL,
+    shipment_id uuid NOT NULL,
     event_type dcsa_v2_0.EventType NOT NULL,
     event_date_time timestamp with time zone NOT NULL,
     event_classifier_code dcsa_v2_0.EventClassifierCode NOT NULL,
     shipment_information_type_code character varying(3) NOT NULL,
-    event_type_code dcsa_v2_0.EventTypeCode NOT NULL,
-    transport_call_id uuid NOT NULL
+    event_type_code dcsa_v2_0.EventTypeCode NOT NULL
 );
 
 DROP TABLE IF EXISTS dcsa_v2_0.transport_call CASCADE;
@@ -118,6 +118,35 @@ CREATE TABLE dcsa_v2_0.transport_event (
     event_classifier_code dcsa_v2_0.EventClassifierCode NOT NULL,
     event_type_code dcsa_v2_0.EventTypeCode NOT NULL
 );
+
+DROP TABLE IF EXISTS dcsa_v2_0.transport_document_type CASCADE;
+CREATE TABLE dcsa_v2_0.transport_document_type (
+    transport_document_type_code integer NOT NULL, -- The type of the transport document (i.e. BOL (bill of lading) or SWB (Sea Waybill).
+    transport_document_type_name varchar(50), -- The full names of the document types (Bill of Lading or Sea Waybill).
+    transport_document_type_description text -- A description of the different docuemtn types.
+);
+
+DROP TYPE IF EXISTS dcsa_v2_0.transport_document_type_code CASCADE;
+CREATE TYPE dcsa_v2_0.transport_document_type_code AS ENUM (
+    'BOL', -- Bill of Lading
+    'SWB' -- Sea Waybill
+);
+
+DROP TABLE IF EXISTS dcsa_v2_0.shipment CASCADE;
+CREATE TABLE dcsa_v2_0.shipment (
+    shipment_id uuid NOT NULL,
+    booking_reference text, -- The identifier for a shipment, which is issued by and unique within each of the carriers.
+    booking_datetime timestamp, -- The date and time of the booking request.
+    transport_document_id varchar(20), -- Transport Document ID is an identifier that links to a shipment. Bill of lading is the legal document issued to the customer which confirms the carrier's receipt of the cargo from the customer acknowledging goods being shipped and specifying the terms of delivery.
+    transport_document_type_code dcsa_v2_0.transport_document_type_code,
+    shipper_name varchar(50), -- The name of the shipper, who requested the booking
+    consignee_name varchar(50), -- The name of the consignee
+    collection_origin varchar(250), -- The location through which the shipment originates. It can be defined as a UN Location Code value or an address. The customer (shipper) needs to place a booking in order to ship the cargo (commodity) from an origin to destination. This attribute specifies the location of the origin.
+    collection_dateTime timestamp, -- The date and the time that the shipment items need to be collected from the origin.
+    delivery_destination varchar(250), -- The location to which the shipment is destined. It can be defined as a UN Location Code value or an address. The customer (shipper) needs to place a booking in order to ship the cargo (commodity) from an origin to destination. This attribute specifies the location of the destination. Also known as 'place of carrier delivery'.
+    delivery_datetime timestamp , -- The date (and when possible time) that the shipment items need to be delivered to the destination.
+    carrier_code varchar(10) -- The Carrier Code represents a concatenation of the Code List Provider Code and the Code List Provider. A hyphen is used between the two codes. The unique carrier identifier is sourced from either the NMFTA SCAC codes list or the SMDG Master Liner codes list.
+    );
 
 ALTER TABLE ONLY dcsa_v2_0.equipment_event
     ADD CONSTRAINT equipment_event_pkey PRIMARY KEY (id);
