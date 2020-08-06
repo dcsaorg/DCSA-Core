d41707e4f03a06d66c2181b27862cef3a27d71a8
Asger Stage <as@asseco.dk>
2020-08-06 10:52:59 +0200

    Add docker IP

--- datamodel/testdata.d/05_test_data.sql
+++ datamodel/testdata.d/05_test_data.sql
@@ -150,3 +150,14 @@ INSERT INTO dcsa_v1_1.event_subscription(
     equipment_reference
 )
     VALUES ('http://localhost:4567/webhook/receive-transport-events', 'TRANSPORT', '', '', '');
+
+
+    INSERT INTO dcsa_v1_1.event_subscription(
+    callback_url,
+    event_type,
+    booking_reference,
+    bill_of_lading_number,
+    equipment_reference
+)
+    VALUES ('http://172.17.0.1:4567/webhook/receive-transport-events', 'TRANSPORT', '', '', '');
+
