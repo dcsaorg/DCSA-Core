1371ddd870d4588e30194d40d70030fb685c6622
Henrik Helmø Larsen <hhl@asseco.dk>
2020-09-04 17:11:02 +0200

    Add filter option to boolean fields (#26)
    * Add filter option to boolean fields
    
    * Check boolean filter value

--- src/main/java/org/dcsa/util/Filter.java
+++ src/main/java/org/dcsa/util/Filter.java
@@ -68,6 +68,14 @@ public class Filter<T> {
                 addExactFilter(fieldName, value, !"NULL".equals(value));
             } else if (Integer.class.equals(fieldType) || Long.class.equals(fieldType)) {
                 addExactFilter(fieldName, value, true);
+            } else if (Boolean.class.equals(fieldType)) {
+                if ("TRUE".equals(value.toUpperCase()) || "FALSE".equals(value.toUpperCase())) {
+                    addExactFilter(fieldName, value, false);
+                } else {
+                    throw new GetException("Boolean filter value must be either: (TRUE|FALSE) - value not recognized: " + value + " on filter: " + fieldType.getSimpleName());
+                }
+            } else {
+                throw new GetException("Type on filter (" + parameter + ") not recognized: " + fieldType.getSimpleName());
             }
         } catch (MethodNotFoundException methodNotFoundException) {
             throw new GetException("Getter method corresponding to parameter: " + parameter + " not found!");
@@ -137,8 +145,8 @@ public class Filter<T> {
         }
     }
 
-    // Transform <, >, </, (�, �), (�, �), �, etc. to html code
-    // # $ ? /� \�
+    // Transform <, >, </, (“, “), (‘, ‘), “, etc. to html code
+    // # $ ? /” \”
     protected String sanitizeValue(String value) {
         if (value != null) {
             // More SQL injection prevention could be done
