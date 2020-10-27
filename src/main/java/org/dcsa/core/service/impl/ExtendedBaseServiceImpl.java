4ba6df7d14fb44087b591686b20f120bf18c2fa2
Henrik Helmø Larsen <hhl@asseco.dk>
2020-10-27 11:48:51 +0100

    Update service

--- src/main/java/org/dcsa/core/service/impl/ExtendedBaseServiceImpl.java
+++ src/main/java/org/dcsa/core/service/impl/ExtendedBaseServiceImpl.java
@@ -1,9 +1,9 @@
 package org.dcsa.core.service.impl;
 
+import org.dcsa.core.extendedrequest.ExtendedRequest;
 import org.dcsa.core.model.GetId;
 import org.dcsa.core.repository.ExtendedRepository;
 import org.dcsa.core.service.ExtendedBaseService;
-import org.dcsa.core.extendedrequest.ExtendedRequest;
 import reactor.core.publisher.Flux;
 
 public abstract class ExtendedBaseServiceImpl<R extends ExtendedRepository<T, I>, T extends GetId<I>, I> extends BaseServiceImpl<R, T, I> implements ExtendedBaseService<T, I> {
@@ -15,7 +15,7 @@ public abstract class ExtendedBaseServiceImpl<R extends ExtendedRepository<T, I>
     public Flux<T> findAllExtended(ExtendedRequest<T> extendedRequest) {
         return getRepository().countAllExtended(extendedRequest).map(
                 count -> {
-                    extendedRequest.setQueryCount(count.getCount()); return count;
+                    extendedRequest.setQueryCount(count); return count;
                 }).thenMany(
                         getRepository().findAllExtended(extendedRequest)
         );
