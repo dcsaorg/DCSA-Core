88e4d625fc9a4d173e336226167901b1ce0e135a
holmHin <35977313+holmHin@users.noreply.github.com>
2021-02-12 16:11:46 +0100

    Feature/fix validation message (#56)
    * Fix IMO Validaion message, expected and found switch
    
    * Fix IMO Validator to check the correct value
    
    * Update version in POM

--- src/main/java/org/dcsa/core/util/ValidationUtils.java
+++ src/main/java/org/dcsa/core/util/ValidationUtils.java
@@ -15,13 +15,13 @@ public class ValidationUtils {
         if (vesselIMONumber != null && vesselIMONumber.length() == 7) {
             int sum = 0;
             for (int i = 0; i < 6; i++) {
-                sum += (7 - i) * (int) vesselIMONumber.charAt(i);
+                sum += (7 - i) * Character.getNumericValue(vesselIMONumber.charAt(i));
             }
             String s = String.valueOf(sum);
             if (vesselIMONumber.charAt(vesselIMONumber.length() - 1) == s.charAt(s.length() - 1)) {
                 return true;
             } else {
-                throw new InvalidParameterException("Invalid Vessel IMO Number. IMO number does not pass checksum - expected value: " + vesselIMONumber.charAt(vesselIMONumber.length() - 1) + " but found: " + s.charAt(s.length() - 1));
+                throw new InvalidParameterException("Invalid Vessel IMO Number. IMO number does not pass checksum - expected value: " +  s.charAt(s.length() - 1) + " but found: " + vesselIMONumber.charAt(vesselIMONumber.length() - 1));
             }
         } else {
             throw new InvalidParameterException("Invalid Vessel IMO Number. Must match 7-digits");
