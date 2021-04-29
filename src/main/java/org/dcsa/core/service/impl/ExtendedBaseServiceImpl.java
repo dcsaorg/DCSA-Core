89572fa78eb5cf95046af78270e81eb1c18dce3e
Niels Thykier <nt@asseco.dk>
2021-04-29 13:55:45 +0200

    Split BaseServiceImpl to better support TO Services
    Services for TO models cannot do "save" and do not have repositories.
    The "save()" method is trivial, just move it out of the interface and
    reduce it to a protected method as it is only used inside services.
    
    With this commit, most TO services can implement the BaseService
    interface and enable their Controller counter part to just extend
    BaseController.
    
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/core/service/impl/ExtendedBaseServiceImpl.java
+++ src/main/java/org/dcsa/core/service/impl/ExtendedBaseServiceImpl.java
@@ -3,25 +3,9 @@ package org.dcsa.core.service.impl;
 import org.dcsa.core.extendedrequest.ExtendedRequest;
 import org.dcsa.core.repository.ExtendedRepository;
 import org.dcsa.core.service.ExtendedBaseService;
-import org.dcsa.core.util.ReflectUtility;
 import reactor.core.publisher.Flux;
 
-public abstract class ExtendedBaseServiceImpl<R extends ExtendedRepository<T, I>, T, I> extends BaseServiceImpl<R, T, I> implements ExtendedBaseService<T, I> {
-
-    private transient Class<T> modelClass;
-
-    @Override
-    public Class<T> getModelClass() {
-        if (modelClass == null) {
-            this.modelClass = ReflectUtility.getConcreteModelClassForService(this.getClass());
-        }
-        return modelClass;
-    }
-
-    @Override
-    public String getType() {
-        return getModelClass().getSimpleName();
-    }
+public abstract class ExtendedBaseServiceImpl<R extends ExtendedRepository<T, I>, T, I> extends BaseRepositoryBackedServiceImpl<R, T, I> implements ExtendedBaseService<T, I> {
 
     public I getIdOfEntity(T entity) {
         return getRepository().getIdOfEntity(entity);
