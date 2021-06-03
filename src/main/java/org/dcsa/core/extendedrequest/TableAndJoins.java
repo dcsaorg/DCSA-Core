4a267bfbae0c824f38d4366c9e2b8fead8aa2fa3
Niels Thykier <nt@asseco.dk>
2021-06-03 10:27:00 +0200

    Remove now unused code
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/core/extendedrequest/TableAndJoins.java
+++ src/main/java/org/dcsa/core/extendedrequest/TableAndJoins.java
@@ -41,41 +41,6 @@ public class TableAndJoins {
         return joins.get(aliasId);
     }
 
-    private static void applyJoinToStringBuilder(StringBuilder sb, JoinDescriptor descriptor) {
-        Column lhs = descriptor.getLHSColumn();
-        Column rhs = descriptor.getRHSColumn();
-        Table table = descriptor.getRHSTable();
-        String joinLine = table.toString() + " ON " + lhs.toString() + "=" + rhs.toString();
-        switch (descriptor.getJoinType()) {
-            case JOIN:
-                sb.append(" JOIN ");
-                break;
-            case LEFT_OUTER_JOIN:
-                sb.append(" LEFT JOIN ");
-                break;
-            case RIGHT_OUTER_JOIN:
-                throw new UnsupportedOperationException("Rewrite to use LEFT JOIN instead");
-            default:
-                throw new UnsupportedOperationException("Unsupported join type: " + descriptor.getJoinType());
-        }
-        sb.append(joinLine);
-    }
-
-    protected void generateFromAndJoins(StringBuilder sb, Set<String> selectedJoinAliases) {
-        int selectedJoins = 0;
-        sb.append(" FROM ");
-        sb.append(primaryTable.toString());
-        for (JoinDescriptor joinDescriptor : joins.values()) {
-            if (selectedJoinAliases.contains(joinDescriptor.getJoinAliasId())) {
-                applyJoinToStringBuilder(sb, joinDescriptor);
-                selectedJoins++;
-            }
-        }
-        if (selectedJoinAliases.size() != selectedJoins) {
-            throw new IllegalArgumentException("selectedJoinAliases contained an unknown alias");
-        }
-    }
-
     public boolean hasJoins() {
         return !joins.isEmpty();
     }
