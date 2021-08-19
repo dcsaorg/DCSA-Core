5753fd5c89341238ccb6f99e13c876893d84394a
Frazer Joe Dourado <frazer.dourado@dcsa.org>
2021-08-19 12:01:40 +0200

    Adjusted enum validator to consider enum lists

--- src/main/java/org/dcsa/core/validator/EnumSubsetValidator.java
+++ src/main/java/org/dcsa/core/validator/EnumSubsetValidator.java
@@ -5,14 +5,15 @@ import org.apache.commons.lang3.NotImplementedException;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 /**
- * Validator for Enumeration subsets. Works both for queryParameters (where the value to validate is a string)
- * and for Enum defined fields (where the value to validate is an Enum)
+ * Validator for Enumeration subsets. Works both for queryParameters (where the value to validate is
+ * a string) and for Enum defined fields (where the value to validate is an Enum)
  *
- * If the value is a string - then this class supports that the string can be comma separated in order to validate
- * a set of values
+ * <p>If the value is a string - then this class supports that the string can be comma separated in
+ * order to validate a set of values
  */
 public class EnumSubsetValidator implements ConstraintValidator<EnumSubset, Object> {
 
@@ -31,10 +32,15 @@ public class EnumSubsetValidator implements ConstraintValidator<EnumSubset, Obje
 
     if (types instanceof String) {
       Stream<String> enumStream =
-              ((String) types).contains(",") ? Arrays.stream(((String) types).split(",")) : Stream.of((String) types);
+          ((String) types).contains(",")
+              ? Arrays.stream(((String) types).split(","))
+              : Stream.of((String) types);
       return enumStream.allMatch(e -> Arrays.asList(subsetTypes).contains(e));
     } else if (types instanceof Enum) {
       return Arrays.asList(subsetTypes).contains(((Enum) types).name());
+    } else if (types instanceof List) {
+      return ((List<? extends Enum>) types)
+          .stream().map(Enum::name).allMatch(e -> Arrays.asList(subsetTypes).contains(e));
     } else {
       throw new NotImplementedException("type not implemented:" + types.getClass().getTypeName());
     }
