a7616a5e3cf93b281457bf68814aeffe8547db04
Frazer Joe Dourado <frazer.dourado@dcsa.org>
2021-09-29 14:27:10 +0200

    Profile based security.

--- src/main/java/org/dcsa/core/configuration/DCSACoreConfiguration.java
+++ src/main/java/org/dcsa/core/configuration/DCSACoreConfiguration.java
@@ -2,11 +2,8 @@ package org.dcsa.core.configuration;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
-import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
 import org.springframework.data.r2dbc.dialect.DialectResolver;
 import org.springframework.data.r2dbc.dialect.R2dbcDialect;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -36,12 +33,4 @@ public class DCSACoreConfiguration {
         log.info("Enabled support for Proxy headers (X-Forwarded-*, etc.) from *any* IP. Use dcsa.supportProxyHeader=[true|false] to change this");
         return new ForwardedHeaderTransformer();
     }
-
-  @EventListener(ApplicationStartedEvent.class)
-  @ConditionalOnExpression(
-      "${dcsa.securityConfig.auth.enabled:false} || T(org.apache.commons.lang3.StringUtils).isEmpty('${spring.security.oauth2.resourceserver.jwt.issuer-uri:}')")
-  void securityLogConfiguration() {
-    log.info(
-        "JWT is disabled as `dcsa.securityConfig.auth.enabled` is false or `spring.security.oauth2.resourceserver.jwt.issuer-uri` is missing.");
-  }
 }
