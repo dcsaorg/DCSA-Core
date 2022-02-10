40f86f9cfeba2c98aac55e4dbea6b307107b5590
Niels Thykier <nt@asseco.dk>
2022-02-10 10:36:23 +0100

    Improve the javadoc of several features
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/core/model/JoinedWithModel.java
+++ src/main/java/org/dcsa/core/model/JoinedWithModel.java
@@ -1,9 +1,67 @@
 package org.dcsa.core.model;
 
+import org.dcsa.core.extendedrequest.ExtendedRequest;
 import org.springframework.data.relational.core.sql.Join;
 
 import java.lang.annotation.*;
 
+/**
+ * Describes join relations that {@link ExtendedRequest} should be aware of
+ *
+ * <b>Note</b>: In most cases, you want {@link ForeignKey} instead of this
+ * annotation.
+ *
+ * Generally speaking, there are two primary use-cases for this annotation
+ * (that are not covered by {@link ForeignKey}).
+ *
+ * <ul>
+ *     <li>Provide filter parameters via JOINs <em>without</em> mapping
+ *     another entity on select in a declarative manner.  Note if you need
+ *     more control over the join then you want a custom {@link ExtendedRequest}
+ *     (and override {@link ExtendedRequest#prepareDBEntityAnalysis()}</li>
+ *     <li>Map in an entity (when combined with {@link MapEntity} on the field
+ *     containing the entity) where the entity cannot be directly joined with
+ *     the original entity (e.g., because you need to join via an "uninteresting"
+ *     intermediate entity that you do not want to have mapped or you want
+ *     a different nesting than what {@link ForeignKey} would have forced on
+ *     the entities).</li>
+ * </ul>
+ *
+ * Example usage (with an "uninteresting" Intermediate entity):
+ *
+ * <pre>{@code
+ *
+ * @Data
+ * @Table("model")
+ * @JoinedWithModel(lhsFieldName = "intermediateId", rhsModel = Intermediate.class, rhsFieldName = "id")
+ * @JoinedWithModel(lhsModel = Intermediate.class, lhsFieldName = "innerModelId", rhsModel = InnerModel.class, rhsFieldName = "id")
+ * public class Model {
+ *
+ *     // ... an @Id field (or other fields) could go here
+ *
+ *     private String intermediateId;
+ *
+ *     @MapEntity
+ *     private InnerModel innerModel;
+ * }
+ *
+ * @Table("intermedate")
+ * class Intermediate {
+ *     @Id
+ *     private String id;
+ *
+ *     private String innerModelId;
+ * }
+ *
+ * @Table("inner_model")
+ * class InnerModel {
+ *     @Id
+ *     private String id;
+ *
+ *     // Other fields of interest here.
+ * }
+ * }</pre>
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Repeatable(ModelJoins.class)
--- src/main/java/org/dcsa/core/model/MapEntity.java
+++ src/main/java/org/dcsa/core/model/MapEntity.java
@@ -1,10 +1,32 @@
 package org.dcsa.core.model;
 
+import org.dcsa.core.extendedrequest.ExtendedRequest;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Make {@link ExtendedRequest} populate this entity
+ *
+ * <b>Note</b>: In most cases, you want {@link ForeignKey} instead of this
+ * annotation.
+ *
+ * The entity must be provided in the underlying SQL query
+ * (usually via @{@link JoinedWithModel}).  Note that in many
+ * cases, this annotation + {@link JoinedWithModel} can often
+ * be replaced by {@link ForeignKey}.  This annotation should
+ * only be used if you need something complex that
+ * {@link ForeignKey} cannot support (like pulling an entity
+ * in over an intermediate link without mapping out the
+ * intermediate entity).
+ *
+ * This annotation only works with 1:1 (JOIN) and 1:0 (LEFT JOIN)
+ * like relations.
+ *
+ * Please see {@link JoinedWithModel} for an example
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface MapEntity {
