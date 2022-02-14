dcsaorg/DCSA-Core

commit 971e3a4cb8f6933f2853b76ece37b7220f31dd5b

Remove `@ModelClass` from ReflectUtils
Signed-off-by: Niels Thykier <nt@asseco.dk>

src/main/java/org/dcsa/core/extendedrequest/QueryFields.java
@@ -1,7 +1,6 @@
 package org.dcsa.core.extendedrequest;
 
 import lombok.*;
-import org.dcsa.core.model.ModelClass;
 import org.dcsa.core.util.ReflectUtility;
 import org.springframework.data.relational.core.sql.Aliased;
 import org.springframework.data.relational.core.sql.Column;
@@ -25,10 +24,6 @@ public class QueryFields {
         Column internalColumn;
         Column selectColumn = null;
         String prefixedJsonName = selectNamePrefix + ReflectUtility.transformFromFieldNameToJsonName(combinedModelField);
-        if (!combinedModelField.isAnnotationPresent(ModelClass.class)) {
-            /* Special-case: Use the primary model when the field does not have a ModelClass */
-            modelClass = originalModelClass;
-        }
         columnName = ReflectUtility.transformFromFieldNameToColumnName(modelClass, combinedModelField.getName());
         internalColumn = table.column(SqlIdentifier.unquoted(columnName));
         if (selectable) {

src/main/java/org/dcsa/core/model/ModelClass.java
@@ -1,19 +1,0 @@
-package org.dcsa.core.model;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.lang.reflect.Field;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
-@Deprecated
-/**
- * @deprecated Not supported any more. Might be fixed in the future.
- */
-public @interface ModelClass {
-    Class<?> value() default Object.class;
-    String fieldName() default "";
-    String viaJoinAlias() default "";
-}

src/main/java/org/dcsa/core/query/impl/AbstractDBEntityAnalysisBuilder.java
@@ -179,14 +179,12 @@ public abstract class AbstractDBEntityAnalysisBuilder<T> implements DBEntityAnal
                         "Model class (RHS side missing). Please use join/chainJoin with a Class parameter if you need this feature.");
             }
             try {
-                lhsColumnName = ReflectUtility.transformFromFieldNameToColumnName(lhsModel, lhsFieldName,
-                        builder.getPrimaryModelClass() == lhsModel
-                );
+                lhsColumnName = ReflectUtility.transformFromFieldNameToColumnName(lhsModel, lhsFieldName);
             } catch (NoSuchFieldException e) {
                 throw new IllegalArgumentException("Unknown field \"" + lhsFieldName + "\" on model " + lhsModel.getSimpleName() + " (khs)");
             }
             try {
-                rhsColumnName = ReflectUtility.transformFromFieldNameToColumnName(rhsModel, rhsFieldName, false);
+                rhsColumnName = ReflectUtility.transformFromFieldNameToColumnName(rhsModel, rhsFieldName);
             } catch (NoSuchFieldException e) {
                 throw new IllegalArgumentException("Unknown field \"" + rhsFieldName + "\" on model " + rhsModel.getSimpleName() + " (rhs)");
             }

src/main/java/org/dcsa/core/query/impl/DefaultDBEntityAnalysisBuilder.java
@@ -398,9 +398,7 @@ public class DefaultDBEntityAnalysisBuilder<T> extends AbstractDBEntityAnalysisB
         try {
             return ReflectUtility.transformFromFieldNameToColumnName(
                     clazz,
-                    fieldName,
-                    /* We do not expect any ModelClass here, as it is not a field from the combined model */
-                    false
+                    fieldName
             );
         } catch (NoSuchFieldException e) {
             throw new IllegalArgumentException("Cannot resolve field " + fieldName + " on class "

src/main/java/org/dcsa/core/util/ReflectUtility.java
@@ -3,7 +3,6 @@ package org.dcsa.core.util;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.dcsa.core.exception.GetException;
-import org.dcsa.core.model.ModelClass;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.data.relational.core.sql.Aliased;
@@ -133,36 +132,13 @@ public class ReflectUtility {
      * @throws NoSuchFieldException if fieldName does not exist on clazz
      */
     public static String transformFromFieldNameToColumnName(Class<?> clazz, String fieldName) throws NoSuchFieldException {
-        return transformFromFieldNameToColumnName(clazz, fieldName, true);
-    }
-
-    /**
-     * Transforms a field name to a column name that can be used by a database query. If no @Column annotation exists
-     * then fieldName is used. If no match on clazz it will try the super class until Object.class is reached
-     *
-     * @param clazz the Class
-     * @param fieldName the name to convert
-     * @param preferModelClassName If true and a {@link ModelClass} annotation exists on the field, then resolve the
-     *                             column name by looking at the {@link ModelClass}.
-     * @return the database column name corresponding to fieldName
-     * @throws NoSuchFieldException if fieldName does not exist on clazz
-     */
-    public static String transformFromFieldNameToColumnName(Class<?> clazz, String fieldName, boolean preferModelClassName) throws NoSuchFieldException {
         Field field = getDeclaredField(clazz, fieldName);
         // Check if this field is declared on another class
-        ModelClass modelClass = field.getDeclaredAnnotation(ModelClass.class);
-        if (preferModelClassName && modelClass != null) {
-            if (modelClass.fieldName().length() > 0) {
-                fieldName = modelClass.fieldName();
-            }
-            return transformFromFieldNameToColumnName(modelClass.value(), fieldName, true);
+        Column column = field.getDeclaredAnnotation(Column.class);
+        if (column != null) {
+            return column.value();
         } else {
-            Column column = field.getDeclaredAnnotation(Column.class);
-            if (column != null) {
-                return column.value();
-            } else {
-                return fieldName;
-            }
+            return fieldName;
         }
     }
 
@@ -267,24 +243,6 @@ public class ReflectUtility {
         clazz = clazz.getSuperclass();
         if (clazz != Object.class) {
             getFieldNamesOfType(clazz, type, fieldNames);
-        }
-    }
-
-
-    /**
-     * Gets the original class corresponding to a field provided on a ModelClass annotation. This
-     * is used when combined modelClasses are in use.
-     *
-     * @param clazz the class to investigate
-     * @param field the field from the class clazz
-     * @return the original class of the fieldName
-     */
-    public static Class<?> getFieldModelClass(Class<?> clazz, Field field) {
-        ModelClass modelClass = field.getAnnotation(ModelClass.class);
-        if (modelClass != null) {
-            return modelClass.value();
-        } else {
-            return clazz;
         }
     }
 

src/test/java/org/dcsa/core/ExtendedRequestTest.java
@@ -180,66 +180,6 @@ public class ExtendedRequestTest {
                 .verify(baseQuery + " WHERE order_table.delivery_date >= :deliveryDate");
     }
 
-    @Disabled("@ModelClass needs to be fixed")
-    @Test
-    public void testOrderByCountry() {
-        String baseQueryNoExtraJoins =
-                "SELECT order_table.orderline AS \"orderline\", customer_table.customer_name AS \"customerName\", customer_address.street_name AS \"customerAddress\", warehouse_address.street_name AS \"warehouse\""
-                + " FROM order_table"
-                + " JOIN customer_table ON order_table.customer_id = customer_table.address_id"
-                + " JOIN address_table customer_address ON customer_table.address_id = customer_address.address_id"
-                + " JOIN address_table warehouse_address ON order_table.address_id = warehouse_address.address_id";
-        String extraJoins = " JOIN city_table ON customer_address.city_id = city_table.id"
-                +  " JOIN country_table ON city_table.country_id = country_table.id";
-        request(OrderInCountry.class, extendedParameters).verify(baseQueryNoExtraJoins);
-
-        request(OrderInCountry.class, extendedParameters)
-                .withParam("countryName", "dk")
-                .verify(baseQueryNoExtraJoins + extraJoins + " WHERE country_table.country_name = :countryName");
-    }
-
-    @Disabled("@ModelClass needs to be fixed")
-    @Test
-    public void testOrderWithEverything() {
-        String baseQueryNoExtraJoins =
-                "SELECT customer_table.customer_name AS \"customerName\", customer_address.street_name AS \"customerAddress\", warehouse_address.street_name AS \"warehouse\", order_table.order_id AS \"id\", order_table.orderline AS \"orderline\", order_table.customer_id AS \"receiverId\", order_table.address_id AS \"warehouseAddressId\", order_table.delivery_date AS \"deliveryDate\""
-                        + " FROM order_table"
-                        + " JOIN customer_table ON order_table.customer_id = customer_table.address_id"
-                        + " JOIN address_table customer_address ON customer_table.address_id = customer_address.address_id"
-                        + " JOIN address_table warehouse_address ON order_table.address_id = warehouse_address.address_id";
-        String extraJoins = " JOIN city_table ON customer_address.city_id = city_table.id"
-                +  " JOIN country_table ON city_table.country_id = country_table.id";
-        request(OrderWithEverything.class, extendedParameters).verify(baseQueryNoExtraJoins);
-
-        request(OrderWithEverything.class, extendedParameters)
-                .withParam("countryName", "dk")
-                .verify(baseQueryNoExtraJoins + extraJoins + " WHERE country_table.country_name = :countryName");
-    }
-
-    @Disabled("@ModelClass needs to be fixed")
-    @Test
-    public void testExtendedOrder() {
-        String query =
-                "SELECT address_table.street_name AS \"warehouse\", order_table.order_id AS \"id\", order_table.orderline AS \"orderline\", order_table.customer_id AS \"receiverId\", order_table.address_id AS \"warehouseAddressId\", order_table.delivery_date AS \"deliveryDate\""
-                        + " FROM order_table"
-                        + " JOIN address_table ON order_table.address_id = address_table.address_id";
-        request(ExtendedOrder.class, extendedParameters).verify(query);
-
-        request(ExtendedOrder.class, extendedParameters)
-                .withParam("deliveryDate[gte]", "2021-01-01T00:00:00Z")
-                .verify(query + " WHERE order_table.delivery_date >= :deliveryDate");
-    }
-
-    @Disabled("@ModelClass needs to be fixed")
-    @Test
-    public void testExtendedOrderDistinct() {
-        String query =
-                "SELECT DISTINCT address_table.street_name AS \"warehouse\", order_table.order_id AS \"id\", order_table.orderline AS \"orderline\", order_table.customer_id AS \"receiverId\", order_table.address_id AS \"warehouseAddressId\", order_table.delivery_date AS \"deliveryDate\""
-                        + " FROM order_table"
-                        + " JOIN address_table ON order_table.address_id = address_table.address_id";
-        request(ExtendedOrder.class, extendedParameters).verify(query, req -> req.setSelectDistinct(true));
-    }
-
     @Test
     public void testQueryBySubclassExtendedRequest() {
         String baseQueryNoExtraJoins =

src/test/java/org/dcsa/core/models/combined/ExtendedOrder.java
@@ -1,11 +1,9 @@
 package org.dcsa.core.models.combined;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import org.dcsa.core.model.JoinedWithModel;
-import org.dcsa.core.model.ModelClass;
 import org.dcsa.core.models.Address;
 import org.dcsa.core.models.Order;
 
@@ -15,7 +13,4 @@ import org.dcsa.core.models.Order;
 @ToString(callSuper = true)
 public class ExtendedOrder extends Order {
 
-    @ModelClass(value = Address.class, fieldName = "address")
-    @JsonProperty("warehouse")
-    private String warehouseAddress;
 }

src/test/java/org/dcsa/core/models/combined/OrderInCountry.java
@@ -1,30 +1,0 @@
-package org.dcsa.core.models.combined;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import org.dcsa.core.model.JoinedWithModel;
-import org.dcsa.core.model.ModelClass;
-import org.dcsa.core.models.*;
-
-@Data
-@EqualsAndHashCode(callSuper = true)
-@JoinedWithModel(lhsFieldName = "receiverId", rhsModel = Customer.class, rhsFieldName = "addressId")
-@JoinedWithModel(lhsModel = Customer.class, lhsFieldName = "addressId", rhsModel = Address.class, rhsJoinAlias = "customer_address", rhsFieldName = "addressId")
-@JoinedWithModel(lhsFieldName = "warehouseAddressId", rhsModel = Address.class, rhsJoinAlias = "warehouse_address", rhsFieldName = "addressId")
-
-/* Joins only present for filtering */
-@JoinedWithModel(lhsJoinAlias = "customer_address", lhsFieldName = "cityId", rhsModel = City.class, rhsFieldName = "id")
-@JoinedWithModel(lhsModel = City.class, lhsFieldName = "countryId", rhsModel = County.class, rhsFieldName = "id", filterFields = {"countryName"})
-public class OrderInCountry extends Order {
-
-    @ModelClass(value = Customer.class, fieldName = "name")
-    private String customerName;
-
-    @ModelClass(fieldName = "address", viaJoinAlias = "customer_address")
-    private String customerAddress;
-
-    @ModelClass(fieldName = "address", viaJoinAlias = "warehouse_address")
-    @JsonProperty("warehouse")
-    private String warehouseAddress;
-}

commit ad21d944a62c0fd87d56c81d768546ea694abce2

Fix regression in query field detection
Regression caused by 971e3a4cb8f6933f2853b76ece37b7220f31dd5b where I
inverted the boolean logic when simplifying out an if-statement.

Note that this makes one of the parameters redundant in the
method. Clean up for that will happen later to avoid stalling the
regression fix (some parameter renaming is in order too).

src/main/java/org/dcsa/core/extendedrequest/QueryFields.java
@@ -24,7 +24,7 @@ public class QueryFields {
         Column internalColumn;
         Column selectColumn = null;
         String prefixedJsonName = selectNamePrefix + ReflectUtility.transformFromFieldNameToJsonName(combinedModelField);
-        columnName = ReflectUtility.transformFromFieldNameToColumnName(modelClass, combinedModelField.getName());
+        columnName = ReflectUtility.transformFromFieldNameToColumnName(originalModelClass, combinedModelField.getName());
         internalColumn = table.column(SqlIdentifier.unquoted(columnName));
         if (selectable) {
             selectColumn = internalColumn.as(SqlIdentifier.quoted(prefixedJsonName));