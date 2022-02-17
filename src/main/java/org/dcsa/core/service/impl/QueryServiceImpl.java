a03e2efddbd23c17a5b694761d8a0f827ec7d9ed
Niels Thykier <nt@asseco.dk>
2022-02-17 10:51:24 +0100

    Split ExtendedBase classes into two
    We need to support for symmetric and asymmetric models between JSON
    vs. data model and this is what this commit does.  However, since the
    `ExtendedBase` name is not very helpful, the classes are also renamed.
    
    The old names is kept around and marked `@Deprecated` to avoid
    breaking existing code when we merge this.
    
    Signed-off-by: Niels Thykier <nt@asseco.dk>

---
+++ src/main/java/org/dcsa/core/service/impl/QueryServiceImpl.java
@@ -0,0 +1,17 @@
+package org.dcsa.core.service.impl;
+
+import org.dcsa.core.extendedrequest.ExtendedRequest;
+import org.dcsa.core.repository.ExtendedRepository;
+import org.dcsa.core.service.QueryService;
+import reactor.core.publisher.Flux;
+
+public abstract class QueryServiceImpl<R extends ExtendedRepository<T, I>, T, I> extends QueryServiceImplSupport<R, T, I> implements QueryService<T, I> {
+
+    @Override
+    public Flux<T> findAllExtended(ExtendedRequest<T> extendedRequest) {
+        return getRepository().countAllExtended(extendedRequest)
+                .doOnNext(extendedRequest::setQueryCount)
+                .thenMany(getRepository().findAllExtended(extendedRequest)
+                );
+    }
+}
